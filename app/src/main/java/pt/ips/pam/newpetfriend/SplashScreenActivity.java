28e7a6dbbbad4d677ccfa200e36c6c2d711fc44b
RicardoNogueiraReis <ricardovreis499@gmail.com>
2022-02-07 22:43:58 +0000

    Splash Screen feita, cores adicionadas para uso futuro e um novo tipo de letra "Poppins"

--- app/src/main/java/pt/ips/pam/newpetfriend/SplashScreenActivity.java
+++ app/src/main/java/pt/ips/pam/newpetfriend/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package pt.ips.pam.newpetfriend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainActivity = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(mainActivity);
+                finish();
+            }
+        },4000);
     }
 }
