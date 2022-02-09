83632fb12d2736dfa0dac7cd973169716bf75889
RicardoNogueiraReis <ricardovreis499@gmail.com>
2022-02-09 10:58:06 +0000

    Adicionada a pagina individual (por fazer), inicio da verficação dos campos no LoginActivity (com strings por fazer)

--- app/src/main/java/pt/ips/pam/newpetfriend/LoginActivity.java
+++ app/src/main/java/pt/ips/pam/newpetfriend/LoginActivity.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText Email;
+    EditText email;
     EditText password;
     Button btnLogin;
     Button btnRegistar;
@@ -18,9 +19,25 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Email = (EditText) findViewById(R.id.editTextEmail);
+        email = (EditText) findViewById(R.id.editTextEmail);
         password = (EditText) findViewById(R.id.editTextPalavraPasse);
         btnLogin = (Button) findViewById(R.id.buttonIniciarSessao);
         btnRegistar = (Button) findViewById(R.id.buttonRegistar);
+
+
+    }
+
+    private boolean verificarCampos(){
+        boolean isEmailEmpty = email.getText().toString().isEmpty();
+        boolean isPasswordEmpty = password.getText().toString().isEmpty();
+
+        if(isEmailEmpty && isPasswordEmpty)
+            Toast.makeText(LoginActivity.this, "@string/erro_email_vazio", Toast.LENGTH_SHORT).show();
+        else if(isEmailEmpty)
+            Toast.makeText(LoginActivity.this, "@string/erro_email_vazio", Toast.LENGTH_SHORT).show();
+        else if(isPasswordEmpty)
+            Toast.makeText(LoginActivity.this, "@string/erro_password_vazia", Toast.LENGTH_SHORT).show();
+
+        return true;
     }
 }
